19bb9ec26de766d611299531a894cfbd6ace45de
Jordan Hollinger <jordan.hollinger@mapquest.com>
2014-01-06 18:54:04 -0500

    Collect group symbolizer columns and handle indexed columns

--- src/agg/process_group_symbolizer.cpp
+++ src/agg/process_group_symbolizer.cpp
@@ -25,6 +25,8 @@
 #include <mapnik/agg_renderer.hpp>
 #include <mapnik/agg_rasterizer.hpp>
 #include <mapnik/image_util.hpp>
+#include <mapnik/feature_factory.hpp>
+#include <mapnik/attribute_collector.hpp>
 
 #include <mapnik/geom_util.hpp>
 #include <mapnik/symbolizer.hpp>
@@ -38,15 +40,57 @@
 #include <string>
 
 // boost
+#include <boost/variant/apply_visitor.hpp>
 
 namespace mapnik {
 
 template <typename T0, typename T1>
 void agg_renderer<T0,T1>::process(group_symbolizer const& sym,
-                              mapnik::feature_impl & feature,
-                              proj_transform const& prj_trans)
+                                  mapnik::feature_impl & feature,
+                                  proj_transform const& prj_trans)
 {
-    // TODO: Process the GroupSymbolizer.
+    // find all column names referenced in the group rules and symbolizers
+    std::set<std::string> columns;
+    attribute_collector column_collector(columns);
+    expression_attributes<std::set<std::string> > rk_attr(columns);
+
+    expression_ptr repeat_key = get<mapnik::expression_ptr>(sym, keys::repeat_key);
+    if (repeat_key)
+    {
+        boost::apply_visitor(rk_attr, *repeat_key);
+    }
+
+    // get columns from child rules and symbolizers
+    group_symbolizer_properties_ptr props = get<group_symbolizer_properties_ptr>(sym, keys::group_properties);
+    if (props) {
+        for (auto const& rule : props->get_rules())
+        {
+            // note that this recurses down on to the symbolizer
+            // internals too, so we get all free variables.
+            column_collector(*rule);
+            // still need to collect repeat key columns
+            if (rule->get_repeat_key())
+            {
+                boost::apply_visitor(rk_attr, *(rule->get_repeat_key()));
+            }
+        }
+    }
+
+    // create a new context for the sub features of this group
+    context_ptr sub_feature_ctx = std::make_shared<mapnik::context_type>();
+
+    // populate new context with column names referenced in the group rules and symbolizers
+    // and determine if any indexed columns are present
+    bool has_idx_cols = false;
+    for (auto const& col_name : columns)
+    {
+        sub_feature_ctx->push(col_name);
+
+        if (col_name.find('%') != std::string::npos)
+        {
+            has_idx_cols = true;
+        }
+    }
 }
 
 template void agg_renderer<image_32>::process(group_symbolizer const&,

4fed8b3063036f886cef9fe0699cc37d40e32fdd
Matt Amos <zerebubuth@gmail.com>
2014-01-15 13:35:24 +0000

    Fix bug in pair layout with a single column.
    Code had assumed that (0, 0) would be the center of the layout
    and represent "no offset", but this isn't the case.

--- src/group/group_layout_manager.cpp
+++ src/group/group_layout_manager.cpp
@@ -82,7 +82,8 @@ struct process_layout : public boost::static_visitor<>
 
         if (member_boxes_.size() == 1)
         {
-            member_offsets_[0] = pixel_position(0, 0);
+            coord<double,2> center = member_boxes_[0].center();
+            member_offsets_[0] = pixel_position(-center.x, -center.y);
             return;
         }
 
